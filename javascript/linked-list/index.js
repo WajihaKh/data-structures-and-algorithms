1786f18e865058c6ebd29625eceff8b0f062660a
Wajiha Khan <jiya.r.kh@gmail.com>
2024-05-03 18:16:54 -0400

    finished code challenge # 5

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,44 @@
 'use strict';
 
-class LinkedList {
-
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
-module.exports = LinkedList;
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+
+  includes(value) {
+    let current = this.head;
+    while (current) {
+      if (current.value === value) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+
+  toString() {
+    let result = '';
+    let current = this.head;
+    while (current) {
+      result += `{ ${current.value} } -> `;
+      current = current.next;
+    }
+    result += 'NULL';
+    return result;
+  }
+}
+
+module.exports = { Node, LinkedList };
