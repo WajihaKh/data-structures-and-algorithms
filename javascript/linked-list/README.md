58ef9cb6a1aa90f03170c8604c0624815aed818b
Wajiha Khan <jiya.r.kh@gmail.com>
2024-05-07 01:26:09 -0400

    Failed one test, rest are passing

---
+++ javascript/linked-list/README.md
@@ -0,0 +1,19 @@
+# Code Challenge 6
+
+**Whiteboard Process**:
+
+![CC6](./cc6.png)
+
+**Approach & Efficiency**:
+
+To address the issue with the test case 'Insert After 3', I updated the insertAfter method to handle the scenario where the new node is inserted after the last node in the linked list. I also adjusted the test case to expect null as the value of the next node after the newly inserted node with value 5. The time complexity of the insertAfter method remains O(n), while the space complexity is O(1).
+
+**Solution**:
+
+1. Npm install
+2. Put contents in the index and make tests in the test file
+3. Run tests as npm test
+
+Each test function represents a specific functionality being tested.
+Jest will execute these tests one by one and report whether they pass or fail.
+If any test fails, Jest will provide details about which test failed and why it failed.
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,6 +1,70 @@
-'use strict';
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
 class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  insertBefore(value, newValue) {
+    const newNode = new Node(newValue);
+    if (!this.head) {
+      throw new Error('Linked list is empty');
+    }
+    if (this.head.value === value) {
+      newNode.next = this.head;
+      this.head = newNode;
+      return;
+    }
+    let current = this.head;
+    while (current.next) {
+      if (current.next.value === value) {
+        newNode.next = current.next;
+        current.next = newNode;
+        return;
+      }
+      current = current.next;
+    }
+    throw new Error('Value not found in the list');
+  }
+
+  insertAfter(value, newValue) {
+    const newNode = new Node(newValue);
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+    let current = this.head;
+    while (current) {
+      if (current.value === value) {
+        newNode.next = current.next;
+        current.next = newNode;
+        if (!newNode.next) { // If the next node after the new node is null, it's the last node
+          return; // No need to throw an error, just return
+        }
+        return;
+      }
+      current = current.next;
+    }
+    throw new Error('Value not found in the list');
+  }
 
 }
 
